226d6d112e54fa933e9067c35b7dfbe6f5504054
Angeliázer Rigon da Silva <yyrigon@hotmail.com>
2024-11-18 18:23:23 -0300

    First1

--- src/database/dataPostgree.js
+++ src/database/dataPostgree.js
@@ -4,12 +4,16 @@ const { Pool } = pkg
 
 config()
 
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASS,
+//   port: process.env.PORT,
+// })
+
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: process.env.PORT,
+  connectionString: process.env.CONNECTING_POSTGRES
 })
 
 export default pool
