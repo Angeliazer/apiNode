94b2c4d4667538f3f02f1f06a40c10133aa258fa
Angeliázer Rigon da Silva <yyrigon@hotmail.com>
2024-11-23 10:47:14 -0300

    commit 4

--- src/database/dataPostgree.js
+++ src/database/dataPostgree.js
@@ -4,16 +4,16 @@ const { Pool } = pkg
 
 config()
 
-const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
-})
-
 //const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL
-// });
+//  user: process.env.PGUSER,
+//  host: process.env.PGHOST,
+//  database: process.env.PGDATABASE,
+//  password: process.env.PGPASSWORD,
+//  port: process.env.PGPORT,
+//})
+
+const pool = new Pool({
+   connectionString: process.env.DATABASE_URL
+ });
 
 export default pool
--- src/repositories/repository.categoria.js
+++ src/repositories/repository.categoria.js
@@ -1,18 +1,18 @@
-import pool from "../database/dataPostgree.js"
+import pool from '../database/dataPostgree.js';
 
 const Listar = async () => {
-  try {
-    const sql = `select * from categoria`
+    try {
+        const sql = `select *
+                     from categoria`;
+        const response = await pool.query(sql);
+        if (response.rows.length !== 0) {
+            return response.rows;
+        } else {
+            return [];
+        }
+    } catch (err) {
+        return [];
+    }
+};
 
-    const response = await pool.query(sql)
-
-    console.log(response)
-
-    if (response.rows.length === 0) return []
-    else return response.rows
-  } catch (err) {
-    return []
-  }
-}
-
-export default { Listar }
+export default {Listar};
