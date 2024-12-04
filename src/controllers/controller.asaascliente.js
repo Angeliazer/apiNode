ae6bfd29d7867da63a7bfa1630628c43fbb42132
Angeliázer Rigon da Silva <yyrigon@hotmail.com>
2024-12-04 16:06:53 -0300

    commit 7

--- src/controllers/controller.asaascliente.js
+++ src/controllers/controller.asaascliente.js
@@ -16,7 +16,7 @@ const Add = (req, res) => {
             }
         });
 
-        res.status(200).json(response);
+        res.status(200).json(response.data.id);
 
     } catch (error) {
         res.status(500).json({error: error});
