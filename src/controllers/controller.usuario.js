595dc35976d009d5bb2e54c1fd36d3b387b267ce
Angeliázer Rigon da Silva <yyrigon@hotmail.com>
2024-12-10 15:23:14 -0300

    commit 7

--- src/controllers/controller.usuario.js
+++ src/controllers/controller.usuario.js
@@ -1,78 +1,76 @@
-import serviceUsuario from "../services/service.usuario.js"
+import serviceUsuario from '../services/service.usuario.js';
 
 async function AddUsuario(req, res) {
-  try {
-    const user = {...req.body}
+    try {
+        const user = {...req.body};
 
-    const usuario = await serviceUsuario.AddUsuario(user)
+        const usuario = await serviceUsuario.AddUsuario(user);
 
-    user.idusuario = usuario.idusuario
-    user.token = usuario.token
-    delete user.password
-
-    if (usuario.idusuario) {
-      res.status(201).json(user);
+        if (usuario.idusuario) {
+            user.idusuario = usuario.idusuario;
+            user.token = usuario.token;
+            delete user.password;
+            res.status(201).json(user);
+        } else
+            res.status(401).json({error: 'Email de Usuário já cadastrado...!'});
+    } catch (error) {
+        res.status(500).json({error: error});
     }
-      else {
-        res.status(401).json({ error: 'Email de Usuário já cadastrado...!' })
-    }
-  } catch (error) {
-    res.status(500).json({ error: error })
-  }
 }
 
 async function Listar(req, res) {
-  try {
-    const usuarios = await serviceUsuario.Listar()
+    try {
+        const usuarios = await serviceUsuario.Listar();
 
-    if (usuarios.length > 0) res.status(200).json(usuarios)
-    else
-      res.status(500).json({ error: "Erro na requisição ao Banco de Dados..." })
-  } catch (error) {
-    res.status(500).json({ status: error })
-  }
+        if (usuarios.length > 0) res.status(200).json(usuarios);
+        else
+            res.status(500).json({error: 'Erro na requisição ao Banco de Dados...'});
+    } catch (error) {
+        res.status(500).json({status: error});
+    }
 }
 
 async function Login(req, res) {
-  try {
-    const {email, password} = req.body
+    try {
+        const {email, password} = req.body;
 
-    const usuario = await serviceUsuario.Login(email, password)
+        const usuario = await serviceUsuario.Login(email, password);
 
-    if (usuario.length !== 0) res.status(200).json(usuario)
-       else
-         res.status(401).json({ error : 'Usuário ou Senha, inválido...!'})
-  } catch (error) {
-    res.status(500).json({ error })
-  }
+        if (usuario.length !== 0) res.status(200).json(usuario);
+        else
+            res.status(401).json({error: 'Usuário ou senha, inválidos...!'});
+    } catch (error) {
+        res.status(500).json({error});
+    }
 }
 
 async function Perfil(req, res) {
-  try {
-    const id_usuario = req.idUsuario
+    try {
+        const id_usuario = req.idUsuario;
 
-    const usuario = await serviceUsuario.Perfil(id_usuario)
+        const usuario = await serviceUsuario.Perfil(id_usuario);
 
-    if (usuario != null) {
-      res.status(200).json(usuario)
-    } else {
-      res.status(404).json({ status: "Registro não encontrado..." })
+        if (usuario != null) {
+            res.status(200).json(usuario);
+        } else {
+            res.status(404).json({status: 'Registro não encontrado...'});
+        }
+    } catch (error) {
+        res.status(500).json({status: error});
     }
-  } catch (error) {
-    res.status(500).json({ status: error })
-  }
 }
 
 async function ListarId(req, res) {
-  try {
-    const idUsuario = req.params
+    try {
+        const idUsuario = req.params;
 
-    const usuario = await serviceUsuario.ListarId(idUsuario.id)
+        const usuario = await serviceUsuario.ListarId(idUsuario.id);
 
-    if (usuario != null) res.status(200).json(usuario)
-    else res.status(404).json({ status: "Registro não encontrado..." })
-  } catch (error) {
-    res.status(500).json({ status: error })
-  }
+        if (usuario != null) res.status(200).json(usuario);
+        else res.status(404).json({status: 'Registro não encontrado...'});
+    } catch (error) {
+        res.status(500).json({status: error});
+    }
 }
-export default { Login, AddUsuario, Listar, ListarId, Perfil }
+
+export default {Login, AddUsuario, Listar, ListarId, Perfil};
